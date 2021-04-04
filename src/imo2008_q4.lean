936ea792a9bd0c0328ab3dd9677bed8bc6118719
Manuel Candales <manuelcandales@gmail.com>
2021-04-04 15:34:21 -0400

    fix name q1 -> q4

--- src/imo2008_q4.lean
+++ src/imo2008_q4.lean
@@ -9,7 +9,7 @@ import data.real.sqrt
 open real
 
 /-!
-# IMO 2008 Q1
+# IMO 2008 Q4
 Find all functions `f : (0,∞) → (0,∞)` (so, `f` is a function from the positive real
 numbers to the positive real numbers) such that
       ```
@@ -21,7 +21,7 @@ for all positive real numbers `w`, `x`, `y`, `z`, satisfying `wx = yz`.
 The desired theorem is that either `f = λ x, x` or `f = λ x, 1/x`
 -/
 
-theorem imo2008_q1
+theorem imo2008_q4
   (f : ℝ → ℝ)
   (H₁ : ∀ x > 0, f(x) > 0)
   (H₂ : ∀ w x y z : ℝ, 0 < w → 0 < x → 0 < y → 0 < z →
